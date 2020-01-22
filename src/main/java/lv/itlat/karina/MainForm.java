ee9fac44cb0c5e509625c817bc4f881f2ab4fa8a
SnowBeLock <karkardro@gmail.com>
2020-01-22 20:41:14 +0200

    OK/CANCEL buttons are working, we have email/phone/name

--- src/main/java/lv/itlat/karina/MainForm.java
+++ src/main/java/lv/itlat/karina/MainForm.java
@@ -1,11 +1,14 @@
 package lv.itlat.karina;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
 
 import java.io.IOException;
 
 public class MainForm extends BorderPane {
+    public TableView<Record> recordsTable;
+
     public MainForm() throws IOException {
         FXMLLoader loader=new FXMLLoader(getClass().getResource("main.fxml"));
         loader.setRoot(this);
@@ -17,11 +20,20 @@ public class MainForm extends BorderPane {
 
 
         DataEntryForm dataEntryWindow=new DataEntryForm(this); //This object can do everything, cause it is it's own owner.
-var data=dataEntryWindow.showAndGet();
+var data=dataEntryWindow.showAndGet(); /// THIS IS THE RECORD ITSELF 0_0 FCK DMN
+
+if(data!=null){
+    recordsTable.getItems().add(data);
+}
     }
     public void Search() {
         System.out.println("Enter your search");
 
+    }
+    public void editRecord(){
+        var selected=recordsTable.getSelectionModel().getSelectedItem();
+        System.out.println(selected.getName());
+       // System.out.println("HAHA"); // here should be a method, that can change created content.
     }
 
 }
